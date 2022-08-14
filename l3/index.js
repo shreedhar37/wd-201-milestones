a1679bae24ba22c4d3c2c4b6f61341f1bbd61a77
Shreedhar Chavan <meshree4@gmail.com>
2022-08-14 20:08:13 +0530

    space removed

--- l3/index.js
+++ l3/index.js
@@ -69,19 +69,19 @@ todos.add({ title: "Pay electric bill", dueDate: tomorrow, completed: false });
 
 console.log("My Todo-list\n\n");
 
-console.log("Overdue\n");
+console.log("Overdue");
 var overdues = todos.overdue();
 var formattedOverdues = todos.toDisplayableList(overdues);
 console.log(formattedOverdues);
 console.log("\n\n");
 
-console.log("Due Today\n");
+console.log("Due Today");
 let itemsDueToday = todos.dueToday();
 let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday);
 console.log(formattedItemsDueToday);
 console.log("\n\n");
 
-console.log("Due Later\n");
+console.log("Due Later");
 let itemsDueLater = todos.dueLater();
 let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater);
 console.log(formattedItemsDueLater);
