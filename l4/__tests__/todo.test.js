647cf3f96d767355c5afa9055619d4ccbd4d3858
Shreedhar Chavan <meshree4@gmail.com>
2022-08-18 21:00:57 +0530

    Added tests for due today and due later

--- l4/__tests__/todo.test.js
+++ l4/__tests__/todo.test.js
@@ -1,6 +1,6 @@
 let todoList = require("../todo.js");
 
-const { all, markAsComplete, add, overDue } = todoList();
+const { all, markAsComplete, add, overDue, dueToday, dueLater } = todoList();
 /* eslint-disable no-undef */
 describe("Todo List Test Suite", () => {
   test("Should add a new todo", () => {
@@ -32,5 +32,31 @@ describe("Todo List Test Suite", () => {
     expect(all.length).toEqual(2);
     overdueItems = overDue();
     expect(overdueItems.length).toBe(1);
+  });
+  test("Should return due today items", () => {
+    expect(all.length).toEqual(2);
+    add({
+      title: "Complete l4 milestone",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    expect(all.length).toBe(3);
+    dueTodayItems = dueToday().filter((item) => item.completed === false);
+    expect(dueTodayItems.length).toBe(1);
+  });
+  test("Should return due later items", () => {
+    expect(all.length).toEqual(3);
+    const today = new Date();
+    const oneDay = 60 * 60 * 24 * 1000;
+    add({
+      title: "Call accountant",
+      completed: false,
+      dueDate: new Date(today.getTime() + 2 * oneDay).toLocaleDateString(
+        "en-CA"
+      ),
+    });
+    expect(all.length).toBe(4);
+    dueLaterItems = dueLater();
+    expect(dueLaterItems.length).toBe(1);
   });
 });
